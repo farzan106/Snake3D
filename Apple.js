1d1e747acc38227d977b3ea6424c63a4332c8385
Syed Farzan Hassan <74588206+farzan106@users.noreply.github.com>
2021-10-13 22:54:42 +0500

    Add files via upload

---
+++ Apple.js
@@ -0,0 +1,25 @@
+class Apple {
+    constructor() {
+      this.node = document.createElement('div');
+      this.node.setAttribute('id', 'apple');
+      board.appendChild(this.node);
+  
+      setApple();
+    }
+  }
+  
+  function setApple() {
+    const appleNode = document.getElementById('apple');
+    const leftLocation = Math.floor(Math.random() * 12) * 50;
+    const topLocation = Math.floor(Math.random() * 12) * 50;
+  
+    for (let i = 0; i < store.body.length; i += 1) {
+      if (leftLocation === store.body[i].left && topLocation === store.body[i].top) {
+        setApple();
+      }
+    }
+  
+    store.apple.left = leftLocation;
+    store.apple.top = topLocation;
+    appleNode.style.transform = `translate(${leftLocation}px, ${topLocation}px) translateZ(50px)`;
+  }
